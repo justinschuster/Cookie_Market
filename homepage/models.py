f7609ddefc034a4f230da96c671688c9434b1076
justinschuster <schujustin@gmail.com>
2019-04-25 17:52:29 -0400

    Sell page now form that takes in info about listing

--- homepage/models.py
+++ homepage/models.py
@@ -1,14 +1,22 @@
 # homepage/models.py
 
 from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 # Model for a Cookie Listing
 class Cookie(models.Model):
     # TODO: Either select what type of cookie on page or here. Don't let user submit custom name, but will decided later.
     cookie_type = models.CharField(max_length=30) 
-    seller = models.CharField(max_length=30)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.IntegerField()
     post_date = models.DateField()
+    # TODO: Need to add a product id system.
 
     def __str__(self):
         return self.cookie_type
+
+class CookieForm(ModelForm):
+    class Meta:
+        model = Cookie
+        fields = ['cookie_type', 'seller', 'price', 'post_date']
--- homepage/views.py
+++ homepage/views.py
@@ -5,7 +5,12 @@ from django.views.generic import TemplateView
 
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+
+from .models import Cookie, CookieForm
+
+
 
 app_name = 'homepage'
 
@@ -22,8 +27,15 @@ def thin_mints(request):
 def tagalongs(request):
     return render(request, 'tagalongs.html', {})
 
-def sell(request):
-    return render(request, 'sell.html', {})
+def sell(request): 
+    if request.method == 'POST':
+        form = CookieForm(request.POST) 
+        if form.is_valid():
+            return redirect('index')
+    else:
+        form = CookieForm()
+
+    return render(request, 'sell.html', {'form': form})
 
 def sign_up(request):
     if request.method == 'POST':
